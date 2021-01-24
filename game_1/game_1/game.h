750eec256f806cad342d0c6ed542365942a1e010
chenlong_cxy <chenlonghyl@163.com>
2021-01-24 19:03:25 +0800

    三子棋

--- game_1/game_1/game.h
+++ game_1/game_1/game.h
@@ -1,12 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #define ROW 3
 #define COL 3
 
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<Windows.h>
 
-void menu();
-
-void game(char board[ROW][COL],int row,int col);
-
-void Displayboard(char board[ROW][COL], int row, int  col);
-
-char checkWin(char board[ROW][COL], int row, int col);
+//初始化棋盘
+void InitBoard(char board[ROW][COL], int row, int col);
+//打印棋盘
+void DisplayBoard(char board[ROW][COL], int row, int col);
+//玩家下棋
+void PlayerMove(char board[ROW][COL], int row, int col);
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col);
+//判断游戏进展情况
+char CheckWin(char board[ROW][COL], int row, int col);
--- game_1/game_1/test.c
+++ game_1/game_1/test.c
@@ -1,20 +1,72 @@
 #include"game.h"
+
+void menu()
+{
+	printf("|----------------------|\n");
+	printf("|        1.play        |\n");
+	printf("|        0.exit        |\n");
+	printf("|----------------------|\n");
+	printf("请输入:>\n");
+}
+
+void game()
+{
+	char ret = 0;
+	char board[ROW][COL];
+	//初始化棋盘，初始化为空格
+	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		system("cls");
+		DisplayBoard(board, ROW, COL);
+		ret = CheckWin(board, ROW, COL);
+		if (ret != 'c')
+			break;
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		system("cls");
+		DisplayBoard(board, ROW, COL);
+		Sleep(1500);
+		ret = CheckWin(board, ROW, COL);
+		if (ret != 'c')
+			break;
+	}
+	if (ret == '#')
+		printf("玩家赢\n");
+	else if (ret == '*')
+		printf("电脑赢\n");
+	else if (ret == 'q')
+		printf("和棋\n");
+	Sleep(1000);
+}
+
 int main()
 {
+	srand((unsigned int)time(NULL));
 	int input = 0;
-	char board[ROW][COL] = ' ';
-	do{
+	do
+	{
 		menu();
+		printf("请选择:>");
 		scanf("%d", &input);
 		switch (input)
 		{
 		case 1:
-			game(board,ROW,COL);
+			system("cls");
+			game();//三子棋的游戏
 			break;
 		case 0:
 			printf("退出游戏\n");
 			break;
+		default:
+			printf("选择错误，请重新选择!\n");
+			break;
 		}
+		system("cls");
 	} while (input);
 	return 0;
 }
