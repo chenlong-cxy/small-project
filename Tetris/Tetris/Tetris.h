77047c3525785001e6c6e2d3a01c4d1933690c51
chenlong-xcy <chenlonghyl@163.com>
2021-08-14 18:45:17 +0800

    俄罗斯方块实现

--- Tetris/Tetris/Tetris.h
+++ Tetris/Tetris/Tetris.h
@@ -1,51 +1,54 @@
-#include <stdio.h>
-#include <Windows.h>
-#include <stdlib.h>
-#include <time.h>
-#include <conio.h>
-
-#define ROW 29 //游戏区行数
-#define COL 20 //游戏区列数
-
-#define DOWN 80 //方向键：下
-#define LEFT 75 //方向键：左
-#define RIGHT 77 //方向键：右
-
-#define SPACE 32 //空格键
-#define ESC 27 //Esc键
-
-struct Face
-{
-	int data[ROW][COL + 10]; //用于标记指定位置是否有方块（1为有，0为无）
-	int color[ROW][COL + 10]; //用于记录指定位置的方块颜色编码
-}face;
-
-struct Block
-{
-	int space[4][4];
-}block[7][4]; //用于存储7种基本形状方块的各自的4种形态的信息，共28
-
-//从文件读取最高分
-void ReadGrade();
-//将最高分写入文件
-void WriteGrade();
-//隐藏光标
-void HideCursor();
-//光标跳转
-void CursorJump(int x, int y);
-//初始化界面
-void InitInterface();
-//初始化方块信息
-void InitBlockInfo();
-//开始游戏
-void StartGame();
-//颜色设置
-void color(int num);
-//画出方块
-void DrawBlock(int shape, int form, int x, int y);
-//空格覆盖
-void DrawSpace(int shape, int form, int x, int y);
-//判断该方块落到(x, y)处是否合法
-int IsLegal(int shape, int form, int x, int y);
-//判断函数
-int JudeFunc();
+//#include <stdio.h>
+//#include <Windows.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <conio.h>
+//#pragma warning (disable:4996)
+//
+//#define ROW 29 //游戏区行数
+//#define COL 20 //游戏区列数
+//
+//#define DOWN 80 //方向键：下
+//#define LEFT 75 //方向键：左
+//#define RIGHT 77 //方向键：右
+//
+//#define SPACE 32 //空格键
+//#define ESC 27 //Esc键
+//
+//struct Face
+//{
+//	int data[ROW][COL + 10]; //用于标记指定位置是否有方块（1为有，0为无）
+//	int color[ROW][COL + 10]; //用于记录指定位置的方块颜色编码
+//}face;
+//
+//struct Block
+//{
+//	int space[4][4];
+//}block[7][4]; //用于存储7种基本形状方块的各自的4种形态的信息，共28种
+//
+////从文件读取最高分
+//void ReadGrade();
+////更新最高分到文件
+//void WriteGrade();
+////隐藏光标
+//void HideCursor();
+////光标跳转
+//void CursorJump(int x, int y);
+////初始化界面
+//void InitInterface();
+////初始化方块信息
+//void InitBlockInfo();
+////开始游戏
+//void StartGame();
+////颜色设置
+//void color(int num);
+////画出方块
+//void DrawBlock(int shape, int form, int x, int y);
+////空格覆盖
+//void DrawSpace(int shape, int form, int x, int y);
+////合法性判断
+//int IsLegal(int shape, int form, int x, int y);
+////判断函数
+//int JudeFunc();
+////主函数
+//int main();
--- Tetris/Tetris/main.c
+++ Tetris/Tetris/main.c
@@ -1,21 +1,20 @@
-#include "Tetris.h"
-int max, grade;
-int main()
-{
-	max = 0, grade = 0;
-	system("title 俄罗斯方块"); //设置cmd窗口的名字
-	system("mode con lines=29 cols=60"); //设置cmd窗口的大小
-	ReadGrade(); //从文件读取最高分
-	HideCursor(); //隐藏光标
-
-	InitInterface(); //初始化界面
-	InitBlockInfo(); //初始化方块信息
-	srand((unsigned int)time(NULL)); //设置随机数生成的起点
-
-	StartGame(); //开始游戏
-
-	return 0;
-}
+//#include "Tetris.h"
+//int max, grade;
+//int main()
+//{
+//	max = 0, grade = 0;
+//	system("title 俄罗斯方块"); //设置cmd窗口的名字
+//	system("mode con lines=29 cols=60"); //设置cmd窗口的大小
+//	ReadGrade(); //从文件读取最高分
+//	HideCursor(); //隐藏光标
+//
+//	InitInterface(); //初始化界面
+//	InitBlockInfo(); //初始化方块信息
+//	srand((unsigned int)time(NULL)); //设置随机数生成的起点
+//
+//	StartGame(); //开始游戏
+//	return 0;
+//}
 
 //#include <conio.h>
 //
